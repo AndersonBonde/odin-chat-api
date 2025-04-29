7b876291d80287166213e8313a4eb512386521b5
Anderson Bonde <andersonbonde@gmail.com>
2025-04-29 11:12:10 -0300

    Add additional checks before saving message to DB

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -21,7 +21,7 @@ const allGeneralMessagesGet = async (req, res) => {
 const createMessagePost = [
   body('text')
     .trim()
-    .isLength({ min: 1 }).withMessage(`Messages can't be blank`)
+    .isLength({ min: 1, max: 1024 }).withMessage(`Message must be between 1 and 1024 characters`)
     .escape(),
   async (req, res) => {
     const { id: userId, text, guestName } = req.body;
@@ -31,7 +31,52 @@ const createMessagePost = [
     if (!errors.isEmpty()) {
       return res.status(400).json({ message: `Error creating a new message for chatRoom with id: ${chatRoomId}`, errors: errors.array() });
     } else {
-      return res.status(201).json({ message: `Creating new message for charRoom with id: ${chatRoomId} WIP` });
+      try {
+        if (userId && guestName){
+          throw new Error('Message cannot have both authorId and guestName');
+        }
+
+        if (!userId && !guestName) {
+          throw new Error('Message must have either authorId or guestName');
+        }
+        
+        const chatRoom = await prisma.chatRoom.findUnique({
+          where: { id: parseInt(chatRoomId) },
+          include: { members: true },
+        });
+        
+        if (!chatRoom) {
+          return res.status(404).json({ message: `chatRoom with id ${chatRoomId} not found` });
+        }
+
+        // User must be a member of the chatRoom before commenting
+        if (userId) {
+          const allowed = chatRoom.members.some((user) => user.id == userId);
+
+          if (!allowed) {
+            return res.status(403).json({ message: `Can't comment on chatRooms that you don't participate` });
+          }
+        }
+
+        // Guests can only comment in General Chat
+        if (guestName && chatRoom.name !== 'General Chat')
+          return res.status(403).json({ message: ' Guests can only comment on General Chat' });
+
+        const newMessage = await prisma.message.create({
+          data: {
+            text,
+            authorId: userId ? parseInt(userId, 10) : null,
+            guestName: userId ? null : (guestName || null),
+            chatRoomId: chatRoom.id,
+          }
+        });
+  
+        return res.status(201).json({ message: `New message for chatRoom with id: ${chatRoomId} created`, newMessage });
+
+      } catch (err) {
+        console.error('Failed to create message with prisma');
+        return res.status(500).json({ message: `Server error creating message`, error: err.message });
+      }
     }
   }
 ]
